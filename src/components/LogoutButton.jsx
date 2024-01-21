52f768759685a2a5b64ab9a61f03ddc2fe62f6c1
pankaj-kb <pankajb2601@gmail.com>
2024-01-21 14:55:10 +0530

    added logout dispatch to logoutbutton

--- src/components/LogoutButton.jsx
+++ src/components/LogoutButton.jsx
@@ -1,18 +1,28 @@
 import axios from "axios";
 import { BASE_URL } from "../api/api";
+import { useDispatch } from "react-redux";
+import {logout} from "../features/authSlice";
 
 const LogoutButton = () => {
+  const dispatch = useDispatch();
+
   const handleLogout = async () => {
     try {
       const response = await axios.post(`${BASE_URL}/users/logout`, {});
-      console.log("After response in Logout.");
       console.log(response.data);
+      if (response.statusCode === 200) {
+        dispatch(logout);
+      }
     } catch (error) {
       console.error(error.message);
     }
   };
 
-  return <button className="bg-black" onClick={handleLogout}>Logout</button>;
+  return (
+    <button className="bg-black" onClick={handleLogout}>
+      Logout
+    </button>
+  );
 };
 
 export default LogoutButton;
