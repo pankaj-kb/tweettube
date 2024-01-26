b15cee833479c33095d5d25ff1c66a14080ac72f
pankaj-kb <pankajb2601@gmail.com>
2024-01-26 13:36:28 +0530

    card element styling doone

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,17 +1,25 @@
 import SideBar from "../components/SideBar";
 import SearchBar from "../components/SearchBar";
+import VideoCard from "../components/VideoCard";
 
 function HomePage() {
   return (
-    <div className="lg:h-screen w-screen flex items-start justify-start bg-accentblack">
+    <div className="flex bg-accentblack min-h-screen">
       <SideBar />
-      <SearchBar
-        placeholder="what's next ??"
-        buttonText="Search"
-        divClassName="lg:flex mt-[1.5%] ml-[30%] items-center justify-center text-accentwhite"
-        inputClassName="bg-accentgray h-[50px] rounded-[25px] w-[500px] text-center font-semibold focus:outline-none text-[20px]"
-        buttonClassName="bg-accentpink h-[50px] rounded-[25px] text-center w-[100px] font-semibold hover:text-accentblack text-[20px] focus:outline-none ml-[-35px]"
-      />
+      <div className="flex flex-col flex-1">
+        <div className="sticky top-0 bg-accentblack p-4">
+          <SearchBar
+            placeholder="what's next ??"
+            buttonText="Search"
+            divClassName="lg:flex items-center justify-center text-accentwhite"
+            inputClassName="bg-accentgray h-[50px] rounded-[25px] w-[400px] text-center font-medium focus:outline-none text-[20px]"
+            buttonClassName="bg-accentpink h-[50px] rounded-[25px] text-center w-[100px] font-medium hover:text-accentblack text-[20px] focus:outline-none ml-[-35px]"
+          />
+        </div>
+        <div className="flex flex-wrap p-8">
+          <VideoCard />
+        </div>
+      </div>
     </div>
   );
 }
