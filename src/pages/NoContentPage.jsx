63d7251dabccba550e04c4a2b10b7c03b90660c0
pankaj-kb <pankajb2601@gmail.com>
2024-02-11 02:51:08 +0530

    added conditional rendering to NoContentPage

--- src/pages/NoContentPage.jsx
+++ src/pages/NoContentPage.jsx
@@ -1,10 +1,16 @@
-function NoContentPage() {
-
-    return(
-        <div className="flex flex-col items-center p-20">
-            <h1 className="text-accentwhite font-bold text-[50px]">Nothing here Yet....</h1>
-        </div>
-    )
+// eslint-disable-next-line react/prop-types
+function NoContentPage({ text }) {
+  return (
+    <div className="flex flex-col items-center p-20">
+      {text ? (
+        <h1 className="text-accentwhite font-bold text-[50px]">{text}</h1>
+      ) : (
+        <h1 className="text-accentwhite font-bold text-[50px]">
+          Nothing here Yet....
+        </h1>
+      )}
+    </div>
+  );
 }
 
-export default NoContentPage
+export default NoContentPage;
