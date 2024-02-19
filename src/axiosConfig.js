2af954a7f74dac4db168ee8cb88da126508fc0d1
pankaj-kb <pankajb2601@gmail.com>
2024-02-19 15:30:21 +0530

    fetching tokens from localstorage

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -5,4 +5,16 @@ import { BASE_URL } from './api/api.js';
 axios.defaults.baseURL = BASE_URL;
 axios.defaults.withCredentials = true;
 
+const accessToken = localStorage.getItem("accessToken");
+const refreshToken = localStorage.getItem("refreshToken");
+
+
+if (accessToken) {
+    axios.defaults.headers.common['Authorization'] = `Bearer ${accessToken}`;
+}
+
+if (refreshToken) {
+    axios.defaults.headers.common["Refresh-Token"] = refreshToken;
+}
+
 export default axios;
